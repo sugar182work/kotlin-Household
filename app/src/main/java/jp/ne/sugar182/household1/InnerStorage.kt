fc54ae6e0fe46fd08fe025d8bddc0e7cb6089fdf
sugar182ray <sugar182ray@gmail.com>
2018-08-23 07:22:11 +0900

    仮アップ

--- app/src/main/java/jp/ne/sugar182/household1/InnerStorage.kt
+++ app/src/main/java/jp/ne/sugar182/household1/InnerStorage.kt
@@ -5,7 +5,7 @@ import java.io.File
 class InnerStorage {
 
     fun saveFile(fileName: String, saveData: String ) {
-
+        // 全然動かない記述
         // ファイルの書き込み
         val writeFile = File(fileName)
         writeFile.writeText(saveData)
